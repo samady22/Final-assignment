d21d26ae8d2712f45a354fc84cf47708928dc72d
Hikmatullah Samady <hikmat.samady1@yahoo.com>
2023-05-13 23:18:40 +0200

    Implement auth
    In this commit I have implemented the authentication for teachers and for the students and I have installed some dependencies.

---
+++ final-assignment/app/Http/Middleware/TeacherMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class TeacherMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            if (Auth::user()->role === "teacher") {
+                return $next($request);
+            } else {
+                return redirect('/')->with("message", "Access denied as you are not teacher.");
+            }
+        } else {
+            return redirect('/login')->with("message", "Login to access the website.");
+        }
+    }
+}
