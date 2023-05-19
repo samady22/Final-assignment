9defc41dac93cc346ad7650e923c6d2ba4a82a46
Mohibullah Sarfiraz <sarfiraz72@gmail.com>
2023-05-19 16:53:37 +0200

    last changes to seeder ad docker

--- final-assignment/docker/enteryp.sh
+++ final-assignment/docker/enteryp.sh
@@ -17,6 +17,7 @@ php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan migrate
+php artisan db:seed --class=TaskSeeder
 
 php artisan serve --port=$PORT --host=0.0.0.0 --env=.env
 exec docker-php-entrypoint "$@"
